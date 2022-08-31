0c9278aa3ae4f40767c748fb44ab165990b2071a
c4hyo <mdcnugroho@student.ce.undip.ac.id>
2022-08-31 09:58:18 +0700

    add date helper

---
+++ lib/helper/date.dart
@@ -0,0 +1,42 @@
+import 'package:intl/intl.dart';
+
+class DateHelper {
+  static String normalDate(DateTime date) {
+    return DateFormat("dd MMMM y").format(date);
+  }
+
+  static String dateWithDay(DateTime date) {
+    return DateFormat("E, dd MMMM y").format(date);
+  }
+
+  static String dateWithTime(DateTime date) {
+    return DateFormat("dd MMMM y H:m").format(date);
+  }
+
+  static String dayOnly(DateTime date) {
+    return DateFormat("E").format(date);
+  }
+
+  static String dateOfDay(DateTime date) {
+    return DateFormat("dd").format(date);
+  }
+
+  static String dateOfMonth(DateTime date) {
+    return DateFormat("MMMM").format(date);
+  }
+
+  static String dateInChatRoom(DateTime date) {
+    if (normalDate(date) == normalDate(DateTime.now())) {
+      return timeOnly(date);
+    } else if (normalDate(date) ==
+        normalDate(DateTime.now().subtract(const Duration(days: 1)))) {
+      return "Yesterday";
+    } else {
+      return DateFormat("MMMM dd").format(date);
+    }
+  }
+
+  static String timeOnly(DateTime date) {
+    return DateFormat("h:m a").format(date);
+  }
+}

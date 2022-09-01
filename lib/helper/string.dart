f6219004e26f18a69e02916658663e79d7ce20c9
c4hyo <mdcnugroho@student.ce.undip.ac.id>
2022-09-01 15:39:07 +0700

    add short rupiah on helper

--- lib/helper/string.dart
+++ lib/helper/string.dart
@@ -21,4 +21,20 @@ class StringHelper {
   static String percentage({required int minNumber, required int maxNumber}) {
     return "${((minNumber / maxNumber) * 100).toStringAsFixed(2)} %";
   }
+
+  static String shortRupiah(int harga) {
+    if (harga.toString().length > 6 && harga.toString().length <= 9) {
+      return "Rp${_removeLastChar(harga.toString(), 6)} Jt";
+    } else if (harga.toString().length > 9 && harga.toString().length <= 12) {
+      return "Rp${_removeLastChar(harga.toString(), 9)} M";
+    } else if (harga.toString().length > 12) {
+      return "Rp${_removeLastChar(harga.toString(), 12)} T";
+    } else {
+      return NumberFormat.simpleCurrency(locale: "id_ID").format(harga);
+    }
+  }
+
+  static String _removeLastChar(String str, int char) {
+    return str.substring(0, str.length - char);
+  }
 }

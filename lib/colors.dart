a37bf0ad042c9deb2ef34c60cc6208e776c80196
c4hyo <mdcnugroho@student.ce.undip.ac.id>
2022-09-08 19:36:02 +0700

    change color

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,157 +5,139 @@
 
 import 'package:flutter/material.dart';
 
-final Color kPrimaryColor = PrimaryColor.c500;
-final Color kDarkColor = SecondaryColor.c900;
-final Color kLightColor = SecondaryColor.c100;
-final Color kSuccessColor = SuccessColor.c500;
-final Color kInfoColor = InfoColor.c500;
-final Color kDangerColor = DangerColor.c500;
-final Color kWarningColor = WarningColor.c500;
+class YoColors {
+  static const MaterialColor primary =
+      MaterialColor(_primaryPrimaryValue, <int, Color>{
+    50: Color(0xFFE1EAF2),
+    100: Color(0xFFB5CADE),
+    200: Color(0xFF84A6C9),
+    300: Color(0xFF5382B3),
+    400: Color(0xFF2E68A2),
+    500: Color(_primaryPrimaryValue),
+    600: Color(0xFF08468A),
+    700: Color(0xFF063D7F),
+    800: Color(0xFF053475),
+    900: Color(0xFF022563),
+  });
+  static const int _primaryPrimaryValue = 0xFF094D92;
 
-class YoColor {
-  static Map<int, Color> primary = {
-    100: rgba(217, 244, 253, 1),
-    200: rgba(180, 230, 252, 1),
-    300: rgba(141, 208, 246, 1),
-    400: rgba(111, 185, 238, 1),
-    500: rgba(66, 152, 228, 1),
-    600: rgba(48, 118, 196, 1),
-    700: rgba(33, 88, 164, 1),
-    800: rgba(21, 62, 132, 1),
-    900: rgba(12, 43, 109, 1),
-  };
-  static Map<int, Color> success = {
-    100: rgba(237, 253, 227, 1),
-    200: rgba(216, 251, 200, 1),
-    300: rgba(186, 244, 170, 1),
-    400: rgba(157, 233, 145, 1),
-    500: rgba(115, 219, 109, 1),
-    600: rgba(79, 188, 82, 1),
-    700: rgba(54, 157, 66, 1),
-    800: rgba(34, 127, 52, 1),
-    900: rgba(20, 105, 44, 1),
-  };
-  static Map<int, Color> info = {
-    100: rgba(218, 254, 254, 1),
-    200: rgba(182, 250, 254, 1),
-    300: rgba(146, 240, 253, 1),
-    400: rgba(118, 225, 251, 1),
-    500: rgba(74, 203, 249, 1),
-    600: rgba(54, 160, 214, 1),
-    700: rgba(37, 122, 179, 1),
-    800: rgba(23, 87, 144, 1),
-    900: rgba(14, 62, 119, 1),
-  };
-  static Map<int, Color> warning = {
-    100: rgba(254, 249, 210, 1),
-    200: rgba(253, 242, 166, 1),
-    300: rgba(250, 231, 120, 1),
-    400: rgba(245, 219, 86, 1),
-    500: rgba(239, 201, 33, 1),
-    600: rgba(205, 168, 24, 1),
-    700: rgba(172, 137, 16, 1),
-    800: rgba(138, 107, 10, 1),
-    900: rgba(114, 86, 6, 1),
-  };
-  static Map<int, Color> danger = {
-    100: rgba(255, 234, 218, 1),
-    200: rgba(255, 208, 181, 1),
-    300: rgba(255, 176, 144, 1),
-    400: rgba(255, 145, 117, 1),
-    500: rgba(255, 95, 71, 1),
-    600: rgba(219, 60, 51, 1),
-    700: rgba(183, 35, 39, 1),
-    800: rgba(147, 22, 35, 1),
-    900: rgba(122, 13, 33, 1),
-  };
-  static Map<int, Color> secondary = {
-    50: rgba(255, 255, 255, 1),
-    100: rgba(224, 233, 244, 1),
-    200: rgba(195, 212, 233, 1),
-    300: rgba(144, 163, 191, 1),
-    400: rgba(89, 103, 128, 1),
-    500: rgba(26, 32, 44, 1),
-    600: rgba(19, 24, 37, 1),
-    700: rgba(13, 18, 31, 1),
-    800: rgba(8, 12, 25, 1),
-    900: rgba(4, 8, 21, 1),
-  };
-}
+  static const MaterialColor primaryAccent =
+      MaterialColor(_primaryAccentValue, <int, Color>{
+    100: Color(0xFF93B1FF),
+    200: Color(_primaryAccentValue),
+    400: Color(0xFF2D67FF),
+    700: Color(0xFF1455FF),
+  });
+  static const int _primaryAccentValue = 0xFF608CFF;
 
-class PrimaryColor {
-  static Color c100 = rgba(217, 244, 253, 1);
-  static Color c200 = rgba(180, 230, 252, 1);
-  static Color c300 = rgba(141, 208, 246, 1);
-  static Color c400 = rgba(111, 185, 238, 1);
-  static Color c500 = rgba(66, 152, 228, 1);
-  static Color c600 = rgba(48, 118, 196, 1);
-  static Color c700 = rgba(33, 88, 164, 1);
-  static Color c800 = rgba(21, 62, 132, 1);
-  static Color c900 = rgba(12, 43, 109, 1);
-}
+  static const MaterialColor danger =
+      MaterialColor(_dangerPrimaryValue, <int, Color>{
+    50: Color(0xFFF9E5E7),
+    100: Color(0xFFF0BFC3),
+    200: Color(0xFFE6949B),
+    300: Color(0xFFDB6972),
+    400: Color(0xFFD44954),
+    500: Color(_dangerPrimaryValue),
+    600: Color(0xFFC72430),
+    700: Color(0xFFC01F29),
+    800: Color(0xFFB91922),
+    900: Color(0xFFAD0F16),
+  });
+  static const int _dangerPrimaryValue = 0xFFCC2936;
 
-class SuccessColor {
-  static Color c100 = rgba(237, 253, 227, 1);
-  static Color c200 = rgba(216, 251, 200, 1);
-  static Color c300 = rgba(186, 244, 170, 1);
-  static Color c400 = rgba(157, 233, 145, 1);
-  static Color c500 = rgba(115, 219, 109, 1);
-  static Color c600 = rgba(79, 188, 82, 1);
-  static Color c700 = rgba(54, 157, 66, 1);
-  static Color c800 = rgba(34, 127, 52, 1);
-  static Color c900 = rgba(20, 105, 44, 1);
-}
+  static const MaterialColor dangerAccent =
+      MaterialColor(_dangerAccentValue, <int, Color>{
+    100: Color(0xFFFFDCDD),
+    200: Color(_dangerAccentValue),
+    400: Color(0xFFFF767A),
+    700: Color(0xFFFF5D62),
+  });
+  static const int _dangerAccentValue = 0xFFFFA9AC;
 
-class InfoColor {
-  static Color c100 = rgba(218, 254, 254, 1);
-  static Color c200 = rgba(182, 250, 254, 1);
-  static Color c300 = rgba(146, 240, 253, 1);
-  static Color c400 = rgba(118, 225, 251, 1);
-  static Color c500 = rgba(74, 203, 249, 1);
-  static Color c600 = rgba(54, 160, 214, 1);
-  static Color c700 = rgba(37, 122, 179, 1);
-  static Color c800 = rgba(23, 87, 144, 1);
-  static Color c900 = rgba(14, 62, 119, 1);
-}
+  static const MaterialColor warning =
+      MaterialColor(_warningPrimaryValue, <int, Color>{
+    50: Color(0xFFFEF9EB),
+    100: Color(0xFFFDF0CE),
+    200: Color(0xFFFBE7AD),
+    300: Color(0xFFF9DD8C),
+    400: Color(0xFFF8D574),
+    500: Color(_warningPrimaryValue),
+    600: Color(0xFFF6C953),
+    700: Color(0xFFF5C249),
+    800: Color(0xFFF3BC40),
+    900: Color(0xFFF1B02F),
+  });
+  static const int _warningPrimaryValue = 0xFFF7CE5B;
 
-class WarningColor {
-  static Color c100 = rgba(254, 249, 210, 1);
-  static Color c200 = rgba(253, 242, 166, 1);
-  static Color c300 = rgba(250, 231, 120, 1);
-  static Color c400 = rgba(245, 219, 86, 1);
-  static Color c500 = rgba(239, 201, 33, 1);
-  static Color c600 = rgba(205, 168, 24, 1);
-  static Color c700 = rgba(172, 137, 16, 1);
-  static Color c800 = rgba(138, 107, 10, 1);
-  static Color c900 = rgba(114, 86, 6, 1);
-}
+  static const MaterialColor warningAccent =
+      MaterialColor(_warningAccentValue, <int, Color>{
+    100: Color(0xFFFFFFFF),
+    200: Color(_warningAccentValue),
+    400: Color(0xFFFFEDCD),
+    700: Color(0xFFFFE4B3),
+  });
+  static const int _warningAccentValue = 0xFFFFFFFF;
 
-class DangerColor {
-  static Color c100 = rgba(255, 234, 218, 1);
-  static Color c200 = rgba(255, 208, 181, 1);
-  static Color c300 = rgba(255, 176, 144, 1);
-  static Color c400 = rgba(255, 145, 117, 1);
-  static Color c500 = rgba(255, 95, 71, 1);
-  static Color c600 = rgba(219, 60, 51, 1);
-  static Color c700 = rgba(183, 35, 39, 1);
-  static Color c800 = rgba(147, 22, 35, 1);
-  static Color c900 = rgba(122, 13, 33, 1);
-}
+  static const MaterialColor success =
+      MaterialColor(_successPrimaryValue, <int, Color>{
+    50: Color(0xFFE9F4EF),
+    100: Color(0xFFC8E3D7),
+    200: Color(0xFFA4D0BC),
+    300: Color(0xFF80BDA1),
+    400: Color(0xFF64AE8C),
+    500: Color(_successPrimaryValue),
+    600: Color(0xFF429870),
+    700: Color(0xFF398E65),
+    800: Color(0xFF31845B),
+    900: Color(0xFF217348),
+  });
+  static const int _successPrimaryValue = 0xFF49A078;
 
-class SecondaryColor {
-  static Color c50 = rgba(255, 255, 255, 1);
-  static Color c100 = rgba(224, 233, 244, 1);
-  static Color c200 = rgba(195, 212, 233, 1);
-  static Color c300 = rgba(144, 163, 191, 1);
-  static Color c400 = rgba(89, 103, 128, 1);
-  static Color c500 = rgba(26, 32, 44, 1);
-  static Color c600 = rgba(19, 24, 37, 1);
-  static Color c700 = rgba(13, 18, 31, 1);
-  static Color c800 = rgba(8, 12, 25, 1);
-  static Color c900 = rgba(4, 8, 21, 1);
-}
+  static const MaterialColor successAccent =
+      MaterialColor(_successAccentValue, <int, Color>{
+    100: Color(0xFFB3FFD5),
+    200: Color(_successAccentValue),
+    400: Color(0xFF4DFF9D),
+    700: Color(0xFF33FF8F),
+  });
+  static const int _successAccentValue = 0xFF80FFB9;
 
-Color rgba(int r, int g, int b, double a) {
-  return Color.fromRGBO(r, g, b, a);
+  static const MaterialColor info =
+      MaterialColor(_infoPrimaryValue, <int, Color>{
+    50: Color(0xFFEBF7FD),
+    100: Color(0xFFCEECF9),
+    200: Color(0xFFADE0F5),
+    300: Color(0xFF8CD3F1),
+    400: Color(0xFF74C9EE),
+    500: Color(_infoPrimaryValue),
+    600: Color(0xFF53BAE9),
+    700: Color(0xFF49B2E5),
+    800: Color(0xFF40AAE2),
+    900: Color(0xFF2F9CDD),
+  });
+  static const int _infoPrimaryValue = 0xFF5BC0EB;
+
+  static const MaterialColor infoAccent =
+      MaterialColor(_infoAccentValue, <int, Color>{
+    100: Color(0xFFFFFFFF),
+    200: Color(_infoAccentValue),
+    400: Color(0xFFB9E4FF),
+    700: Color(0xFFA0DAFF),
+  });
+  static const int _infoAccentValue = 0xFFECF8FF;
+
+  static const MaterialColor neutral =
+      MaterialColor(_neutralPrimaryValue, <int, Color>{
+    50: Color(0xFFE5E5E8),
+    100: Color(0xFFBEBFC5),
+    200: Color(0xFF92949E),
+    300: Color(0xFF666977),
+    400: Color(0xFF46485A),
+    500: Color(_neutralPrimaryValue),
+    600: Color(0xFF212437),
+    700: Color(0xFF1B1E2F),
+    800: Color(0xFF161827),
+    900: Color(0xFF0D0F1A),
+  });
+  static const int _neutralPrimaryValue = 0xFF25283D;
 }
